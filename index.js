dd73502ec8aa8c9ec99735d42069b342c0ff3942
Asif Estiak <me.asif.ae@gmail.com>
2023-07-25 12:29:32 +0600

    new code added

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.oxnofiz.mongodb.net/?retryWrites=true&w=majority`;
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "Unauthorized" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.secret_key, (err, decode) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: "Unauthorized" });
+    }
+    req.decoded = decode;
+    next();
+  });
+};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,9 +39,19 @@ async function run() {
   try {
     const classCollection = await client.db("languageHub").collection("class");
     const userCollection = await client.db("languageHub").collection("user");
+    const messageCollection = await client
+      .db("languageHub")
+      .collection("message");
     const feedbackMessageCollection = await client
       .db("languageHub")
       .collection("message");
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.secret_key, { expiresIn: "3h" });
+
+      res.send({ token });
+    });
 
     // save user info
     app.post("/userinfo", async (req, res) => {
@@ -45,10 +71,14 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/classes", async (req, res) => {
+    app.get("/classes", verifyJWT, async (req, res) => {
       const email = req.query?.email;
       if (!email) {
         res.status(404).send({ message: "Email Not Found", error: true });
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(401).send({ error: true, message: "Unauthorized" });
       }
       const result = await classCollection.find({ email: email }).toArray();
       res.send(result);
@@ -112,6 +142,18 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/message/:id", async (req, res) => {
+      const id = req.params.id;
+      const isFeedbackExists = await messageCollection.findOne({ uid: id });
+      if (isFeedbackExists) {
+        const results = await messageCollection.findOne({ uid: id });
+        res.send(results);
+    
+      } else {
+        return res.send({ message: "Feedback Not Found", error: true });
+      }
+    });
+
     app.get("/filterByPopularClass", async (req, res) => {
       const results = await classCollection
         .aggregate([
@@ -148,11 +190,52 @@ async function run() {
       res.send(results);
     });
 
+    // delete booked Class
     app.delete("/booked/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const results = await classCollection.deleteOne(query);
       res.send(results);
+    });
+
+    // find data by id
+    app.get("/class/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const results = await classCollection.findOne(query);
+      res.send(results);
+    });
+
+    // update class data
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const data = req.body;
+
+      const option = { upsert: true };
+      const updatetask = {
+        $set: {
+          ...data,
+        },
+      };
+
+      const result = await classCollection.updateOne(query, updatetask, option);
+  
+      res.send(result);
+    });
+
+    // add booked data
+    app.post("/message", async (req, res) => {
+      const message = req.body;
+      const existsMessage = await messageCollection.findOne({
+        uid: message.uid,
+      });
+      if (existsMessage) {
+        return res.send({ message: "Already Feedback Send", error: true });
+      } else {
+        const results = await messageCollection.insertOne(message);
+        return res.send(results);
+      }
     });
 
     await client.db("admin").command({ ping: 1 });
